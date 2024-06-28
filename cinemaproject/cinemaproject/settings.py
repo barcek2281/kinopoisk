9dcbebb9816358d03f3b2e9ef1396ff4654fefe5
barcek2281 <sabdpp17@gmail.com>
2024-06-29 03:46:14 +0500

    modify apps and settings

--- cinemaproject/cinemaproject/settings.py
+++ cinemaproject/cinemaproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
--- cinemaproject/movies/models.py
+++ cinemaproject/movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    created_year = models.DateField()
+    rating = models.IntegerField() 
+    avatar = models.ImageField()
+    box_office = models.BigIntegerField()
+    budgets = models.BigIntegerField()
+
+
