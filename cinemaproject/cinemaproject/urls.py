edb50cf0784822f3f82923b767cf46e894e5003b
Ernar <khanapin65@gmail.com>
2024-06-30 16:33:26 +0500

    base templates, base views, json settings

--- cinemaproject/cinemaproject/urls.py
+++ cinemaproject/cinemaproject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('', include('main.urls')),
 ]
