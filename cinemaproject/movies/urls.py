ca48bb72a1e879688a46d2f459cf05d2aff9b520
Ernar <bwjson27@gmail.com>
2024-07-06 16:19:52 +0500

    initial commit

--- cinemaproject/movies/urls.py
+++ cinemaproject/movies/urls.py
@@ -5,4 +5,5 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.catalog, name='catalog'),
+	path('movie/<str:name>', views.movie, name='movie')
 ]
--- cinemaproject/movies/views.py
+++ cinemaproject/movies/views.py
@@ -3,6 +3,9 @@ from .models import Movie
 # Create your views here.
 
 def catalog(request):
-
     movies_list = Movie.objects.all()
     return render(request, 'movies/catalog.html', {'movies': movies_list})
+
+def movie(request, name):
+    movie = Movie.objects.filter(name=name)
+    return render(request, 'movies/movie.html', {movie:movie})
