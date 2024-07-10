df8f47e44a50807f716f20080c3ecd152f01a07a
barcek2281 <sabdpp17@gmail.com>
2024-07-10 23:52:17 +0300

    user registration

--- cinemaproject/main/admin.py
+++ cinemaproject/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+User = get_user_model()
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
